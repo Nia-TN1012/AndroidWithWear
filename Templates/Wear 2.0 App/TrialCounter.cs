fa54322ceeb64daca3cda4948e0cb3b3775e1745
Nia-TN1012 <nia1012-tmnk@outlook.jp>
2018-02-27 16:27:30 +0900

    Add Wear 2.0 App template.

---
+++ Templates/Wear 2.0 App/TrialCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Note: Code placed on this shared project, can be used for both mobile and wearable projects.
+
+namespace $safeprojectname$ {
+
+	/// <summary>
+	///		Simple counter
+	/// </summary>
+	public class TrialCounter {
+
+		/// <summary>
+		///		Gets current count.
+		/// </summary>
+		public int Count { get; private set; } = 0;
+
+		/// <summary>
+		///		Count up.
+		/// </summary>
+		public void CountUp() => Count++;
+
+		/// <summary>
+		///		Resets counter.
+		/// </summary>
+		public void Reset() => Count = 0;
+
+	}
+}
