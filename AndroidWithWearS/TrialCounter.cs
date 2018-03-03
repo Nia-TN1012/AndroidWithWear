5d0573e98190a2826d397984234afdf4e9a48f14
Nia-TN1012 <nia1012-tmnk@outlook.jp>
2018-03-03 13:37:31 +0900

    Add Android with Wear 2.0 App (.NET Standard) multi-project.

---
+++ AndroidWithWearS/TrialCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidWithWearS {
+
+	/// <summary>
+	///		Simple counter
+	/// </summary>
+	public class TrialCounter {
+
+		/// <summary>
+		///		Gets current count.
+		/// </summary>
+		public int Count { get; private set; } = 0;
+
+		/// <summary>
+		///		Count up.
+		/// </summary>
+		public void CountUp() => Count++;
+
+		/// <summary>
+		///		Resets counter.
+		/// </summary>
+		public void Reset() => Count = 0;
+
+	}
+}
