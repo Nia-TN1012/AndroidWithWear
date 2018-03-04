25c1716cf5bf538355cf29d1ebfa04c184991995
Nia-TN1012 <nia1012-tmnk@outlook.jp>
2018-03-04 23:45:50 +0900

    Add template items.

---
+++ Templates/.NET Standard/AndroidWithWearS.Mobile/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace $safeprojectname$ {
+
+	[Activity( Label = "@string/app_name", MainLauncher = true )]
+	public class MainActivity : Activity {
+
+		/// <summary>
+		///		Counter of shared project.
+		/// </summary>
+		public TrialCounter counter;
+
+		/// <summary>
+		///		Invoked when the activity is first created.
+		/// </summary>
+		/// <param name="bundle">Saved instance state</param>
+		protected override void OnCreate( Bundle bundle ) {
+			base.OnCreate( bundle );
+
+			counter = new TrialCounter();
+
+			// Set our view from the "main" layout resource
+			SetContentView( Resource.Layout.Main );
+
+			// Sets button's click event.
+			var button = FindViewById<Button>( Resource.Id.myButton );
+			button.Click += ( sender, e ) => {
+				counter.CountUp();
+				button.Text = $"{counter.Count} times tapped.";
+			};
+		}
+	}
+}
+
